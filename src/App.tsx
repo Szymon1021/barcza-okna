a40548c2f6b588abac4818f508693810aef4132f
Szymon Kokot <105548800+Szymon1021@users.noreply.github.com>
2026-09-13 16:06:45 +0200

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { HashRouter as Router, Routes, Route } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Offer from "./pages/Offer";
 import Contact from "./pages/Contact";
 
-const publicUrl = process.env.PUBLIC_URL || "";
-const redirectedRoute = new URLSearchParams(window.location.search).get("route");
-
-if (redirectedRoute) {
-  window.history.replaceState(null, "", `${publicUrl}${redirectedRoute}`);
-}
-
 const App: React.FC = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL || "/"}>
+    <Router>
       <div className="flex flex-col min-h-screen" id="barczak-okna">
         <Navbar />
         <main className="flex-grow">
